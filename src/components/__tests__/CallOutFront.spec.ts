25ea702aed48a45018c75cd1141fd3ee5ff93001
Adrian Garcia <canonale@gmail.com>
2023-03-07 16:50:00 +0100

    Slogans were added to the call out component

--- src/components/__tests__/CallOutFront.spec.ts
+++ src/components/__tests__/CallOutFront.spec.ts
@@ -3,6 +3,8 @@ import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import CallOutFront from "../callOutFront/CallOutFront.vue";
 import Logotype from "../callOutFront/Logotype.vue";
+import GradientTitle from "../callOutFront/GradientTitle.vue";
+import ButtonLine from "../callOutFront/ButtonLine.vue";
 
 describe("Menu Top", () => {
   it("The component exists", () => {
@@ -12,5 +14,15 @@ describe("Menu Top", () => {
   it("Logotype exists", () => {
     const wrapper = mount(CallOutFront);
     expect(wrapper.findComponent(Logotype).exists()).toBe(true);
+  });
+  it("Gradient title exists", () => {
+    const slotText = "Canonale";
+    const wrapper = mount(GradientTitle, {
+      slots: {
+        default: slotText,
+      },
+    });
+    expect(wrapper.findComponent(GradientTitle).exists()).toBe(true);
+    expect(wrapper.find("h1").text()).eq(slotText);
   });
 });
