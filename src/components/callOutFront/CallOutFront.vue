5d1cb88004d093d8a11ae4ef5cd18dc9aed1214e
Adrian Garcia <canonale@gmail.com>
2023-04-02 18:46:50 +0200

    MD call-out done

--- src/components/callOutFront/CallOutFront.vue
+++ src/components/callOutFront/CallOutFront.vue
@@ -5,15 +5,15 @@ import ButtonLine from "./ButtonLine.vue";
 </script>
 <template>
   <div class="my-20">
-    <div>
-      <logotype />
+    <div class="flex justify-center">
+      <Logotype />
     </div>
     <div class="mt-10 flex flex-col gap-y-11">
       <gradient-title>Canonale</gradient-title>
-      <h1 class="text-5xl text-slate-200 leading-tight">
+      <h1 class="md:text-center text-5xl text-slate-200 leading-tight">
         Next Generation Management Tooling
       </h1>
-      <h2 class="text-slate-400 text-2xl">
+      <h2 class="md:text-center text-slate-400 text-2xl">
         Get ready for a full-stack engineer so that you can finally catch up with your limits.
       </h2>
       <button-line />
--- src/components/callOutFront/GradientTitle.vue
+++ src/components/callOutFront/GradientTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="main-title">
+  <h1 class="main-title md:text-center">
     <slot></slot>
   </h1>
 </template>
