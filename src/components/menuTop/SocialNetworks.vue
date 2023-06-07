13922cff2025d1ff76f2e4a9121c49f8b9c82c40
Adrian Garcia <canonale@gmail.com>
2023-06-07 20:41:30 +0200

    Go to my github profile button added

--- src/components/menuTop/SocialNetworks.vue
+++ src/components/menuTop/SocialNetworks.vue
@@ -2,11 +2,21 @@
 import IconTwitter from "../icons/IconTwitter.vue";
 import IconDiscord from "../icons/IconDiscord.vue";
 import IconGithub from "../icons/IconGithub.vue";
+
+const goToGitHub = () => {
+  window.location.href = "https://github.com/canonale";
+};
 </script>
 <template>
   <div class="flex">
-    <icon-twitter class="mr-2 transition text-slate-600 hover:text-slate-400" />
-    <icon-discord class="mr-2 transition text-slate-600 hover:text-slate-400" />
-    <icon-github class="transition text-slate-600 hover:text-slate-400" />
+    <icon-twitter class="mr-2 network-icon" />
+    <icon-discord class="mr-2 network-icon" />
+    <icon-github @click="goToGitHub" class="network-icon" />
   </div>
 </template>
+
+<style>
+.network-icon {
+  @apply transition text-slate-600 hover:text-slate-400 cursor-pointer;
+}
+</style>
