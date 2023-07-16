f0c14b0f69211fef9f608568a1de73b86a9b4da6
Adrian Garcia <canonale@gmail.com>
2023-07-16 13:34:50 +0200

    The header is fixed when user scrolling

--- src/components/header/HeaderMain.vue
+++ src/components/header/HeaderMain.vue
@@ -1,17 +1,47 @@
 <script setup lang="ts">
 import MenuTop from "@/components/menuTop/MenuTop.vue";
 import LogoType from "@/components/logotype/LogoType.vue";
+import { ref } from "vue";
+
+const headerHeightLimit = 0;
+const defaultClass = "header";
+const effectClass = "fixed-header";
+const headerClasses = ref<string[]>([defaultClass]);
+
+const handleScroll = () => {
+  headerClasses.value =
+    window.scrollY > headerHeightLimit
+      ? [defaultClass, effectClass]
+      : [defaultClass];
+};
+
+window.addEventListener("scroll", handleScroll);
 </script>
 
 <template>
-  <div
-    class="flex p-2 justify-between items-center dark:bg-gray-800 z-20 relative"
-  >
-    <div>
-      <logo-type />
-    </div>
-    <div>
-      <menu-top />
+  <div :class="headerClasses" id="header">
+    <div class="header-container">
+      <div>
+        <logo-type />
+      </div>
+      <div>
+        <menu-top />
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.header {
+  @apply fixed top-0 left-0 right-0 z-20;
+  @apply transition-colors duration-500;
+}
+.header-container {
+ @apply flex p-2 justify-between items-center;
+ @apply max-w-[1376px] m-auto;
+}
+.fixed-header {
+  @apply bg-white border-b border-gray-200;
+  @apply dark:bg-gray-800 dark:border-gray-900;
+}
+</style>
