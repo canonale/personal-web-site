31e76599301ef613a59e34a36918bd7c253de50a
Adrian Garcia <canonale@gmail.com>
2023-07-17 12:48:14 +0200

    Move route names to a constant

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+export const RouteNames = {
+  HOME: "home",
+  ABOUT: "about",
+  ABOUT_ME: "about-me",
+};
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
+      name: RouteNames.HOME,
       component: HomeView,
     },
     {
       path: "/about",
-      name: "about",
+      name: RouteNames.ABOUT,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
