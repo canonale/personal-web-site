146224c8da29ce8da33fa85ed2b8fb98900bded3
Adrian Garcia <canonale@gmail.com>
2023-07-17 13:01:51 +0200

    It uses routing to deal with pages

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
-import CallOutFront from "./components/callOutFront/CallOutFront.vue";
-import FeatureCardCollection from "./components/featureCard/FeatureCardCollection.vue";
-import HeaderMain from "./components/header/HeaderMain.vue";
-import SalesCardCollection from "./components/salesCard/SalesCardCollection.vue";
-import ContactForm from "./components/forms/forms/ContactForm.vue";
-import FooterWeb from "./components/footer/FooterWeb.vue";
+import HeaderMain from "@/components/header/HeaderMain.vue";
+import FooterWeb from "@/components/footer/FooterWeb.vue";
 </script>
 
 <template>
   <div>
     <header-main />
-    <div class="mx-2">
-      <call-out-front />
-      <feature-card-collection />
-      <sales-card-collection />
-    </div>
+    <router-view />
     <div>
-      <contact-form />
       <footer-web />
     </div>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import CallOutFront from "@/components/callOutFront/CallOutFront.vue";
+import FeatureCardCollection from "@/components/featureCard/FeatureCardCollection.vue";
+import SalesCardCollection from "@/components/salesCard/SalesCardCollection.vue";
+import ContactForm from "@/components/forms/forms/ContactForm.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <div class="mx-2">
+      <call-out-front />
+      <feature-card-collection />
+      <sales-card-collection />
+    </div>
+    <div>
+      <contact-form />
+    </div>
+  </div>
 </template>
