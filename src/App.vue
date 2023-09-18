1d6d37c465be4c8b892322121bd284defd67696e
Adrian Garcia <canonale@gmail.com>
2023-09-18 21:24:07 +0200

    Refactor cookie banner. Code was move into a new component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import useCookies from "./components/cookieBanner/useCookies";
 import HeaderMain from "@/components/header/HeaderMain.vue";
 import FooterWeb from "@/components/footer/FooterWeb.vue";
-const { showBanner, okClicked } = useCookies();
+import CookieBanner from "@/components/cookieBanner/CookieBanner.vue";
 </script>
 
 <template>
@@ -16,20 +15,8 @@ const { showBanner, okClicked } = useCookies();
     <div>
       <footer-web />
     </div>
-    <transition name="slide">
-      <div v-if="showBanner" class="banner-container">
-        <p>We use 🍪 for more details check our policy
-          <router-link to="cookiesPolicy">here</router-link>
-        </p>
-        <button @click="okClicked">OK</button>
-      </div>
+    <transition name="fade">
+      <cookie-banner />
     </transition>
   </div>
 </template>
-
-<style scoped>
-.banner-container {
-  @apply fixed bg-white p-8 w-80 right-10 bottom-10 rounded-lg z-20 drop-shadow-lg;
-  @apply dark:bg-gray-800 dark:text-gray-100;
-}
-</style>
---
+++ src/components/cookieBanner/CookieBanner.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import ButtonCta from "@/components/button/ButtonCta.vue";
+import useCookies from "./useCookies";
+import RouteNames from "@/router/routeNames";
+const { showBanner, okClicked } = useCookies();
+</script>
+
+<template>
+  <div v-if="showBanner" class="banner-container">
+    <p>
+      We use 🍪 for more details check our policy
+      <router-link :to="RouteNames.ABOUT_ME">here</router-link>
+    </p>
+    <div class="flex flex-row-reverse">
+      <button-cta cta @click="okClicked">OK</button-cta>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.banner-container {
+  @apply fixed bg-white p-8 mx-4 bottom-10 rounded-lg z-20 drop-shadow-lg;
+  @apply sm:right-10 sm:w-80;
+  @apply dark:bg-gray-800 dark:text-gray-100;
+}
+</style>
